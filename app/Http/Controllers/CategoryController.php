25e60c225fe45821dc68ac5768a55afd280cae54
Govind Singh <govindmehra176@gmail.com>
2024-03-24 23:05:54 +0530

    git push

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,6 +15,32 @@ use Illuminate\Support\Str;
 class CategoryController extends Controller
 {
     
+    public function getCategories()
+    {
+        try {
+            $categories = Category::select('id', 'name', 'status', 'slug', 'image')->get();
+            return response()->json([
+                'success' => true,
+                'data' => CategoryResource::collection($categories),
+                'meta'=>[
+                    'table'=>[
+                    "id",
+                    "name",
+                    "status",
+                    "image",
+                    "created_at",
+                    "updated_at"
+                    ]
+                ]
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error fetching categories',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
     public function storeCategory(Request $request)
     {
         $validator=Validator::make($request->all(),[
